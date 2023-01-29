93db723201126b52ace0bbb2a1cfa7b7a9772c77
jordan Moldovan <jmoldyvan@gmail.com>
2023-01-28 19:25:15 -0800

    gen buttons style

--- client/src/Components/Main/Generations.js
+++ client/src/Components/Main/Generations.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 export default function Generations(props){
 
@@ -9,22 +10,25 @@ const highlight = {
 
 return(
     <div className="">
-        {/* {props.genertaions.map((elementObject, i) =>{
-            return (
-                <div>
-                    {elementObject[`gen${i+1}`] ? <button onClick= {() => props.toggleGeneration(`gen${i+1}`)} style={highlight}>{`gen${i+1}`}</button> : <button onClick= {() => props.toggleGeneration(`gen${i+1}`)} >{`gen${i+1}`}</button>}
-                </div>
-            )
-        })} */}            
-        {props.genertaions[0].selected ? <button onClick= {() => props.toggleGeneration('gen1')} style={highlight}>Gen 1</button> : <button onClick= {() => props.toggleGeneration('gen1')} >Gen 1</button>}
-        {props.genertaions[1].selected ? <button onClick= {() => props.toggleGeneration('gen2')} style={highlight}>Gen 2</button> : <button onClick= {() => props.toggleGeneration('gen2')} >Gen 2</button>}
-        {props.genertaions[2].selected ? <button onClick= {() => props.toggleGeneration('gen3')} style={highlight}>Gen 3</button> : <button onClick= {() => props.toggleGeneration('gen3')} >Gen 3</button>}
-        {props.genertaions[3].selected ? <button onClick= {() => props.toggleGeneration('gen4')} style={highlight}>Gen 4</button> : <button onClick= {() => props.toggleGeneration('gen4')} >Gen 4</button>}
-        {props.genertaions[4].selected ? <button onClick= {() => props.toggleGeneration('gen5')} style={highlight}>Gen 5</button> : <button onClick= {() => props.toggleGeneration('gen5')} >Gen 5</button>}
-        {props.genertaions[5].selected ? <button onClick= {() => props.toggleGeneration('gen6')} style={highlight}>Gen 6</button> : <button onClick= {() => props.toggleGeneration('gen6')} >Gen 6</button>}
-        {props.genertaions[6].selected ? <button onClick= {() => props.toggleGeneration('gen7')} style={highlight}>Gen 7</button> : <button onClick= {() => props.toggleGeneration('gen7')} >Gen 7</button>}
-        {props.genertaions[7].selected ? <button onClick= {() => props.toggleGeneration('gen8')} style={highlight}>Gen 8</button> : <button onClick= {() => props.toggleGeneration('gen8')} >Gen 8</button>}
-        {props.genertaions[8].selected ? <button onClick= {() => props.toggleGeneration('gen9')} style={highlight}>Gen 9</button> : <button onClick= {() => props.toggleGeneration('gen9')} >Gen 9</button>}
+        <TransitionGroup tag="div" name="animate-options" className={`poke-options-1`}>
+
+            {/* {props.genertaions.map((elementObject, i) =>{
+                return (
+                    <div>
+                        {elementObject[`gen${i+1}`] ? <button onClick= {() => props.toggleGeneration(`gen${i+1}`)} style={highlight}>{`gen${i+1}`}</button> : <button onClick= {() => props.toggleGeneration(`gen${i+1}`)} >{`gen${i+1}`}</button>}
+                    </div>
+                )
+            })} */}            
+            <div className={`poke-options-1-button ${!props.genertaions[0].selected ? 'selected' : ''}`} onClick= {() => props.toggleGeneration('gen1')}><button>Gen 1</button></div>
+            <div className={`poke-options-1-button ${!props.genertaions[1].selected ? 'selected' : ''}`} onClick= {() => props.toggleGeneration('gen2')}><button>Gen 2</button></div>
+            <div className={`poke-options-1-button ${!props.genertaions[2].selected ? 'selected' : ''}`} onClick= {() => props.toggleGeneration('gen3')}>Gen 3</div>
+            <div className={`poke-options-1-button ${!props.genertaions[3].selected ? 'selected' : ''}`} onClick= {() => props.toggleGeneration('gen4')}>Gen 4</div>
+            <div className={`poke-options-1-button ${!props.genertaions[4].selected ? 'selected' : ''}`} onClick= {() => props.toggleGeneration('gen5')}>Gen 5</div>
+            <div className={`poke-options-1-button ${!props.genertaions[5].selected ? 'selected' : ''}`} onClick= {() => props.toggleGeneration('gen6')}>Gen 6</div>
+            <div className={`poke-options-1-button ${!props.genertaions[6].selected ? 'selected' : ''}`} onClick= {() => props.toggleGeneration('gen7')}>Gen 7</div>
+            <div className={`poke-options-1-button ${!props.genertaions[7].selected ? 'selected' : ''}`} onClick= {() => props.toggleGeneration('gen8')}>Gen 8</div>
+            <div className={`poke-options-1-button ${!props.genertaions[8].selected ? 'selected' : ''}`} onClick= {() => props.toggleGeneration('gen9')}>Gen 9</div>
+        </TransitionGroup>
     </div>
 )
 }
