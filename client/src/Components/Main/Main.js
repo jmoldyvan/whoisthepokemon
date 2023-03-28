7dbb79240b897e2d88867ed8ae97eaa7769d0f25
jordan Moldovan <jmoldyvan@gmail.com>
2023-03-27 18:41:13 -0700

    bug fix

--- client/src/Components/Main/Main.js
+++ client/src/Components/Main/Main.js
@@ -118,10 +118,11 @@ function giveUpRevealAnswer(){
             <h3>Who's That Pokemon?</h3>
             {randomPokemonInfo ? <img style={pokemonShadow} src={randomPokemonInfo.sprites.other['official-artwork']['front_default']}></img> : <img></img>}
             {winCondition && <h3>It's {randomPokemonInfo.name}</h3>}
-            {/* <AutocompleteSearch
+            <AutocompleteSearch
                 pokemonNames={pokemonNames}
                 takeGuess={takeGuess}
-            /> */}
+                winCondition={winCondition}
+            />
             <button 
                 style={oppositeStyles} 
                 className="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900" 
