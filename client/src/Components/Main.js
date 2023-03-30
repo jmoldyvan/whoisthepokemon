8294e6cf9d1504a01dda5c3bbb502be1954ed2f7
jordan Moldovan <jmoldyvan@gmail.com>
2023-03-30 16:10:13 -0700

    changes to autocomplete mui

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -1,15 +1,17 @@
 import React, {useState, useEffect} from "react";
 import { useAuth } from "../contexts/AuthContext";
 import Logout from "./Logout";
-import { Link, useNavigate } from "react-router-dom";
-import { updateScore, getUserScore, updateHighScore, getUserHighScore } from "./Services";
+import { useNavigate } from "react-router-dom";
+import { updateScore, updateHighScore, getUserHighScore } from "./Services";
 import { getPokemonInfo, allPokemonNames } from "./APIService";
 import Leaderboard from "./Leaderboard";
-import { ReactSearchAutocomplete } from "react-search-autocomplete";
+import AutocompleteSearch from "./AutocompleteSearch"
+// need mui autocomplete
+// npm install @mui/material @emotion/react @emotion/styled
 
 export default function Main(){
 
-    const { currentUser} = useAuth();
+    const { currentUser } = useAuth();
     const navigate = useNavigate();
     const [randomPokemonInfo, setRandomPokemonInfo] = useState()
     const [winCondition, setWinCondition] = useState(false)
@@ -17,9 +19,7 @@ export default function Main(){
     const [leaderboardBool, setLeaderboardBool] = useState(false)
     const [comboTracker, setcomboTracker] = useState(0)
     const [highscoreTracker, setHighScoreTracker] = useState(0)
-    const [formData, setFormData] = useState({firstGuess: ""})
     const [pokemonNames, setPokemonNames] = useState()
-    const [searchString, setSearchString] = useState("");
 
 async function generateRandomPokemon(){
     try {
@@ -33,7 +33,6 @@ async function generateRandomPokemon(){
   }
 
   randomPokemonInfo?console.log(randomPokemonInfo.name):console.log(null)
-//   randomPokemonInfo?console.log(randomPokemonInfo):console.log(null)
 
 async function allNamesOfPokemon() {
     try {
@@ -98,18 +97,6 @@ const pokemonShadow = {
     pointerEvents : "none"
 }
 
-function handleChange(event) {
-    setFormData(prevFormData => {
-        return {
-            ...prevFormData,
-            [event.target.name]: event.target.value.toLowerCase()
-        }
-    })
-}
-function handleSubmit(event) {
-    event.preventDefault()
-}
-
 function toggleLeaderboard() {
     setLeaderboardBool(prevValue => !prevValue)
 }
@@ -119,52 +106,33 @@ function giveUpRevealAnswer(){
     setcomboTracker(0)
 }
 
-const handleOnSearch = (string, results) => {
-    console.log(string, results);
-    setSearchString(string);
-};
-const handleOnSelect = (item) => {
-    setSearchString(item.name);
-};
-console.log(searchString);
     return(
         <div>
             {currentUser ? <h3>Hello {currentUser.displayName}</h3> : <h3>Hello</h3>}
             <h3>Who's That Pokemon?</h3>
-            {randomPokemonInfo ? 
-            <img style = {pokemonShadow} src={randomPokemonInfo.sprites.other['official-artwork']['front_default']}></img> : 
-            <img></img>}
+            {randomPokemonInfo ? <img style={pokemonShadow} src={randomPokemonInfo.sprites.other['official-artwork']['front_default']}></img> : <img></img>}
             {winCondition && <h3>It's {randomPokemonInfo.name}</h3>}
-            <form onSubmit={handleSubmit}>
-                <div style={{ width: 200, margin: 20 }}>
-                    <ReactSearchAutocomplete
-                        items={pokemonNames}
-                        onSearch={handleOnSearch}
-                        onSelect={handleOnSelect}
-                        inputSearchString={searchString}
-                        showIcon={false}
-                        maxResults={5}
-                        autoFocus
-                        onChange={handleChange}
-                        name="firstGuess"
-                        value={formData.firstGuess}
-                    />
-                </div>
-                <button onClick={()=> takeGuess(searchString)}   
-                    style={styles}  
-                    className="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">TAKE GUESS
-                </button>
-            </form>                
-                <button style={oppositeStyles} className="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900" onClick={generateRandomPokemon} >Next Pokemon</button>
-                <button
-                    style={styles}  
-                    className="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900" 
-                    onClick={giveUpRevealAnswer} 
-                >Give Up</button>
-                <h1>You Have {comboTracker} combo</h1>
-                <h1>Your HighScore {highscoreTracker}</h1>
-                {leaderboardBool ? <Leaderboard handleLeaderboard = {toggleLeaderboard} /> : <button onClick={() => toggleLeaderboard()}>Leaderboard</button>}
-                <Logout />
+            <AutocompleteSearch
+                pokemonNames={pokemonNames}
+                takeGuess={takeGuess}
+
+            />
+            <button 
+                style={oppositeStyles} 
+                className="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900" 
+                onClick={generateRandomPokemon} 
+                >Next Pokemon
+            </button>
+            <button
+                style={styles}  
+                className="w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900" 
+                onClick={giveUpRevealAnswer} 
+                >Give Up
+            </button>
+            <h1>You Have {comboTracker} combo</h1>
+            <h1>Your HighScore {highscoreTracker}</h1>
+            {leaderboardBool ? <Leaderboard handleLeaderboard={toggleLeaderboard} /> : <button onClick={() => toggleLeaderboard()}>Leaderboard</button>}
+            <Logout />
         </div>
     )
 }
