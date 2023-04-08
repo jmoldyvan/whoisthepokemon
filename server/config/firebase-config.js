ae1b44a259e4f7fb898afb74750f696c76f04598
jordan Moldovan <jmoldyvan@gmail.com>
2023-04-07 18:22:06 -0700

    finsished product!

--- server/config/firebase-config.js
+++ server/config/firebase-config.js
@@ -1,7 +1,11 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getAuth } from "firebase-admin/auth";
 
-import serviceAccountKey from './serviceAccountKey.json' assert { type: "json" };
+const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
+
+const serviceAccountKey = loadJSON('./serviceAccountKey.json');
+
+// import serviceAccountKey from './serviceAccountKey.json' assert { type: "json" };
  
 const app = initializeApp({
     credential: cert(serviceAccountKey),
